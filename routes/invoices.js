550dca77033321b01122854dc45755545b3a132f
Ruby Hoseinzadeh <robina.sre78@gmail.com>
2021-07-13 15:15:42 -0700

    BizTime exercise

---
+++ routes/invoices.js
@@ -0,0 +1,17 @@
+const { request } = require("express");
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+router.get('/', async(req, res, next) => {
+    try {
+        const results = await db.query(`SELECT * FROM invoices`);
+        return res.json({ invoices: results.rows} )
+    } catch(e) {
+        return next(e)
+    }
+})
+
+
+module.exports = router;
